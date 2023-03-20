69083ee87b5a4f8be6cdd7490b574a3098471cb3
Neha Singh <Neha.Singh@clinisys.com>
2023-03-20 10:27:39 +0530

    html(loginform)

--- healthcare.js
+++ healthcare.js
@@ -3,45 +3,69 @@ console.log("Javascript working...")
 function validateform() {
     var name1 = document.myform.FirstName.value;
     name1 = name1.trim();
-    var name2 = document.myform.FirstName.value;
+    var name2 = document.myform.LastName.value;
     name2 = name2.trim();
 
     var passwordd = document.myform.password.value;
-    var confpass =  document.myform.confirmpassword.value;
+    var confpass = document.myform.confirmpassword.value;
     var mail = document.myform.mailId.value;
-    var atposition = mail.indexOf("@");
-    var dotposition = mail.lastIndexOf(".");
 
-    if (name1 == null || name1 == "") {
-        alert("Name can't be blank");
+    if (validatename(name1) == true && validatename(name2) == true)
+        return true;
+
+
+
+    else if (validatePasswords(passwordd, confpass) != true) {
+        alert("Password length should be atleast 8 and should be less than 15 and password and confirm password should be same");
         return false;
     }
 
-    else if (name2 == null || name2 == "") {
-        alert("Name can't be blank");
+
+    if (validatemailId(mail) != true) {
+        alert("Enter your mailId correctly");
         return false;
-    }
-
-
-    else if (passwordd.length < 6 ) {
-        alert("Password must be 6 characters long");
-        return false;
-    }
-
-    else if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= mail.length) {
-        alert("Please enter a valid e-mail address \n atpostion:" + atposition + "\n dotposition:" + dotposition);
-        return false;
-    }
-
-    else if(passwordd !=confpass){
-      alert("Your Password did not match");
-       return false;
     }
 
     return true;
+
 }
 
 
+
+//validating name
+function validatename(name) {
+    const pattern = /^[A-Za-z ]^/;
+    if (pattern.test(name) != true)
+        alert("Password length should be atleast 8 and should be less than 15 and password and confirm password should be same");
+
+
+}
+
+//validate password
+function validatePasswords(passwordd, confpass) {
+
+    if (passwordd.length >= 8 || passwordd.length <= 15) {
+
+        if (passwordd == confpass)
+            return true;
+
+    }
+}
+
+
+function validatemailId(mail) {
+    var ispresent = mail.indexOf("@");
+    if (ispresent < 1) return false;
+
+    var dot = mail.indexOf(".");
+    if (dot <= ispresent + 2) return false;
+
+    // check that the dot is not at the end
+    if (dot === mail.length - 1) return false;
+
+    return true;
+
+}
 
 
 
